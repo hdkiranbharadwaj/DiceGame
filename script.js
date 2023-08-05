09ef83c81cc2051b127787d616296da4c1bc0e14
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-08-05 22:31:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,20 +28,14 @@ function showGameInstructions() {
   const instructions = `Welcome to the Dice Game!
 
   How to Play:
-  - This intriguing game involves two players who take turns rolling a dice.
-  - The ultimate objective is for a player to achieve a precise total score of 20 points.
-  - During your turn, gracefully tap the dice to initiate the roll.
-  - In the event of rolling a one, alas, all accumulated points during that turn shall be relinquished, and the honor of the next turn shall be bestowed upon the opposing player.
-  - Upon rolling any other number, a conundrum arises, presenting you with two choices:
-    a. Embrace the fortune's favor and roll again, adding the newly rolled number to your current turn's score.
-    b. Wisely opt to Hold, permitting the accumulation of points from your present turn to be gracefully added to your overall score, signifying the commencement of the other player's turn.
-  - The pursuit of points can endure, as players maintain the liberty to continue rolling until they choose to Hold or encounter the fateful roll of a one.
-  - Proceed with care, for only when you declare to Hold, the bounty of points shall be credited to your total score.
-  - A momentous decree awaits those whose total score crosses the threshold of 20; they shall be proclaimed the unfortunate loser, granting the title of victor to the opposing player.
+  - Two players take turns rolling a dice.
+  - Goal: Be the first to reach an exact total of 20 points.
+  - Roll a one, lose your turn's points, and the other player goes next.
+  - Roll other numbers, choose to roll again or hold to add points.
+  - Continue rolling until you hold or roll a one.
+  - The player whose score exceeds 20 loses, declaring the other player the winner.
 
-  Embrace the elegance of this game and savor the thrill of strategy and chance in each turn!
-
-  Best wishes and enjoy the game!`;
+  Enjoy the excitement and have fun playing!`;
 
   alert(instructions);
 }
