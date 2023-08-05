c18bd7dcb1d9bdebcddc02d216f750a7a5b21b34
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-08-05 20:53:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] == 20) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
